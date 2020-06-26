1095a54e0837afe1d099992ab7b085beb0283adb
Davyd McColl <davydm@gmail.com>
2020-06-26 14:54:19 +0200

    :zap: speed up tests by splitting them out so jest can run them in parallel

---
+++ tests/git-broadcast/broadcast-to-all-but-source-by-default.spec.ts
@@ -0,0 +1,54 @@
+import "expect-even-more-jest";
+import { Sandbox } from "filesystem-sandbox";
+import * as faker from "faker";
+import { gitBroadcast } from "../../src/git-broadcast";
+import { Repository } from "../repository";
+
+describe(`git-broadcast`, () => {
+    beforeEach(() => {
+        // we're doing fs-ops here, give things a little more
+        // time (though they generally won't need it)
+        jest.setTimeout(60000);
+    });
+
+    afterEach(async () => await Sandbox.destroyAll());
+
+    it(`should broadcast to all that aren't the source by default`, async () => {
+        // Arrange
+        const
+            sandbox = await Sandbox.create(),
+            featureBranch = "feature/stuff",
+            readmeContents = `initial: ${ faker.random.words() }`,
+            updatedContents = `updated: ${ faker.random.words() }`,
+            initialMessage = ":tada: initial commit",
+            updatedMessage = ":memo: prior docs are all wrong!",
+            originPath = await sandbox.mkdir("origin"),
+            localPath = await sandbox.mkdir("local"),
+            origin = Repository.create(originPath);
+        await origin.init();
+        await sandbox.writeFile("origin/readme.md", readmeContents);
+        await origin.commitAll(initialMessage);
+        await sandbox.writeFile("origin/readme.md", updatedContents);
+        await origin.commitAll(updatedMessage);
+        await origin.checkout("master");
+
+        const local = await Repository.clone(originPath, localPath);
+        await local.fetch();
+        await local.resetHard("HEAD~1");
+        await local.checkout("-b", featureBranch);
+
+        const currentContents = await sandbox.readTextFile("local/readme.md");
+        expect(currentContents)
+            .toEqual(readmeContents); // should have readme reset
+        // Act
+        await gitBroadcast({
+            in: localPath,
+            from: "master"
+        })
+        // Assert
+        await local.checkout(featureBranch);
+        const log = await local.log();
+        expect(log.latest.message)
+            .toEqual(updatedMessage);
+    });
+});
